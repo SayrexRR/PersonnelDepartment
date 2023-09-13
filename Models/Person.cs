6a68849b5377c449b2b646946007174bee6ab935
Oleksandr <sashazinchenko2000@gmail.com>
2023-09-13 11:42:23 +0300

    Added new functionality

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PersonnelDepartment
+namespace PersonnelDepartment.Models
 {
-    public class MyTable
+    public class Person
     {
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
-        public string Faculty { get; set; }
-        public string Group { get; set; }
-        public string Department { get; set; }
+        public string Gender { get; set; }
+        public Passport PassportData { get; set; }
+        public string Address { get; set; }
+        public List<Person>? Childrens { get; set; }
     }
 }
---
+++ View/PersonsView.cs
@@ -0,0 +1,24 @@
+﻿using PersonnelDepartment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.View
+{
+    public class PersonsView
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Faculty { get; set; }
+        public string Group { get; set; } = string.Empty;
+        public string Department { get; set; }
+        public Person? Parent { get; set; }
+        public List<Person>? Childrens { get; set; }
+        public bool IsStudent { get; set; } = false;
+        public bool IsTeacher { get; set; } = false;
+        public bool IsDepartmentHead { get; set; } = false;
+    }
+}
